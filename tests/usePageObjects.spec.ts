ccc1b2798dc30e982871d708be6527d94fc6543b
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-06-22 13:09:20 -0700

    #78 agros ci - fix

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -38,7 +38,6 @@ test('parametrized methods', async({page}) => {
 })
 
 test.only('testing with argos ci', async ({ page }) => {
-    
     const pm = new PageManager(page)
     await pm.navigateTo().formLayoutsPage()
     await argosScreenshot(page, "form layouts page");
