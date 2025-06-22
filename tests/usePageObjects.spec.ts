d15a3d5ec96c95d6a031b05adf6f485a829436d7
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-06-21 23:07:41 -0700

    commented in argos ci

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -38,7 +38,7 @@ test('parametrized methods @smoke', async({page}) => {
 test.only('testing with argos ci', async({page}) => {
     const pm = new PageManager(page)
     await pm.navigateTo().formLayoutsPage()
-    await argosScreenshot(page, "form layouts page");
+    // await argosScreenshot(page, "form layouts page");
     await pm.navigateTo().datepickerPage()
-    await argosScreenshot(page, "datepicker page");
+    // await argosScreenshot(page, "datepicker page");
 })
